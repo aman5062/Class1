01dc553362330c757a2c1178399d0a3f9e749a4c
aman5062 <aman9122701848@gmail.com>
2024-06-15 20:41:29 +0530

    df

---
+++ operator.js
@@ -0,0 +1,13 @@
+// let age = 18;
+// let warning;
+// if(age>=18){
+//     warning = "You can enter";
+// }else{
+//     warning ="You can't enter";
+// }
+// console.log(warning);
+
+let age = 18;
+let warning;
+age>=18 ? (warning = "You can enter") : (warning ="You can't enter");
+console.log(warning);
