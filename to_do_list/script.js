7d74209d6dc2bc22e4d1907c0ab80e8e9e1b95f1
aman5062 <aman9122701848@gmail.com>
2024-07-02 20:13:08 +0530

    df

--- to_do_list/script.js
+++ to_do_list/script.js
@@ -1,11 +1,12 @@
+let num = 0;
 function addOne(){
     var task = document.getElementById("task_value").value;
     let data = "<div class='box_con'><div class='box_name'>"+task+"</div><div class='box_com' onclick='del(this)'>X</div></div>";
-
+    document.cookie="data"+num+"="+task;
 
     document.getElementById("todo_box").innerHTML += data; 
     document.getElementById("task_value").value = "";
-
+    num++;
 }
 function del(elem){
     var task_com = elem.parentElement.innerText.split("\n")[0];
